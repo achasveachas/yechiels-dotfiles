70f4d3b3bf9082b66917cfd2fc614a2e07ba4731
Yechiel K <contact@yechiel.me>
2019-02-14 20:34:22 -0500

    fix prompt format

--- .prompt.sh
+++ .prompt.sh
@@ -8,4 +8,4 @@ parse_git_branch() {
  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
-PS1="${debian_chroot:+($debian_chroot)}\$(if [ \$? == 0 ]; then echo 😀; else echo 😱; fi)\[\033[01;34m\]\u@\h\[\033[00m\]:\[\033[01;32m\]\w\[\033[01;31m\]\$(parse_git_branch)\[\033[00m\] \$ "
+PS1="${debian_chroot:+($debian_chroot)}\$(if [ \$? == 0 ]; then echo 😀; else echo 😱; fi)\[\033[01;34m\] \u@\h\[\033[00m\]:\[\033[01;32m\]\w\[\033[01;31m\]\$(parse_git_branch)\[\033[00m\] \$ "
