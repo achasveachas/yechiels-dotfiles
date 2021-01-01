c47b183a401fbf364c3f8829f226f03662812b9b
Yechiel K <contact@yechiel.me>
2020-12-31 21:26:02 -0500

    add yeet as an alias

--- .aliases.sh
+++ .aliases.sh
@@ -2,6 +2,7 @@ alias connect="sudo easytether-usb"
 alias vpn="globalprotect"
 alias screen="/snap/screen/current/screen/Screen"
 alias please="sudo "
+alias yeet="rm -rf "
 
 mk() {
 	mkdir $1 && cd $1
