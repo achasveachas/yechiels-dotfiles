dae1b7167e4667a1249bb4ec3c6a8d6d3ba62370
Yechiel Kalmenson <contact@yechiel.me>
2025-04-29 15:24:28 -0400

    fix emoji behavior in prompt

--- .prompt.sh
+++ .prompt.sh
@@ -1,11 +1,22 @@
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
+PROMPT_COMMAND=__prompt_command 
 
-# Add git branch if its present to PS1
 parse_git_branch() {
  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
-PS1="${debian_chroot:+($debian_chroot)}\[\033[01;34m\]\u@\h\[\033[00m\]:\[\033[01;32m\]\w\[\033[01;31m\]\$(parse_git_branch)\[\033[00m\]\n\$(if [ \$? == 0 ]; then echo 😀; else echo 😱; fi) \$ "
+__prompt_command() {
+    local EXIT="$?"
+
+    # set variable identifying the chroot you work in (used in the prompt below)
+    if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+        local debian_chroot=$(cat /etc/debian_chroot)
+    fi
+
+    if [ "$EXIT" == "0" ]; then 
+        local status_emoji=😀; 
+    else 
+        local status_emoji=😱; 
+    fi
+
+    PS1="${debian_chroot:+($debian_chroot)}\[\033[01;34m\]\u@\h\[\033[00m\]:\[\033[01;32m\]\w\[\033[01;31m\]\$(parse_git_branch)\[\033[00m\]\n${status_emoji} \$ "
+}
